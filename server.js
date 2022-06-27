f2147e71251655fce57c1eedef59d7d25998d304
eyang <emily.young0327@gmail.com>
2022-06-27 10:37:10 -0400

    setup server

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get('/', (req, res) =>{
+  res.send('welcome!')
+})
+
+const port = process.env.PORT || 8000
+
+app.listen(port, ()=>{console.log(`Server is listening on port ${port}`);})
