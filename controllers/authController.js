e147c2cfd9b624251f524778ddfcd047bbca7e07
eyang <emily.young0327@gmail.com>
2022-06-29 20:48:40 -0400

    adding json web token

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,7 +15,17 @@ const register = async (req, res) => {
   }
 
   const user = await User.create({ name, email, password });
-  res.status(StatusCodes.CREATED).json({ user });
+  const token = user.createJWT();
+  res.status(StatusCodes.CREATED).json({
+    user: {
+      email: user.email,
+      lastName: user.lastName,
+      location: user.location,
+      name: user.name,
+    },
+    token,
+    location: user.location,
+  });
 };
 
 const login = async (req, res) => {
@@ -24,7 +34,7 @@ const login = async (req, res) => {
 
 const updateUser = async (req, res) => {
   res.send("updateUser");
-  User.save()
+  User.save();
 };
 
 export { register, login, updateUser };
