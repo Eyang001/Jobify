14e0c2c5050c07d59083372c25bb41169b0abb83
eyang <emily.young0327@gmail.com>
2022-07-04 17:31:55 -0400

    cleanup

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -15,12 +15,7 @@ const Landing = () => {
           <h1>
             job <span>Tracking</span> app
           </h1>
-          <p>
-            I'm baby wayfarers hoodie next level taiyaki brooklyn cliche blue
-            bottle single-origin coffee chia. Aesthetic post-ironic venmo,
-            quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch
-            narwhal.
-          </p>
+
           <Link to="/register" className="btn btn-hero">
             Login/Register
           </Link>
